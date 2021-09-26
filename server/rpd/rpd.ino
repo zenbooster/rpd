8db6fecd2077a0258a348de3d8f005b086db8afb
zenbooster <zenbooster@gmail.com>
2021-09-26 14:07:02 +0300

    Оставим пока SAMPLE_RATE 2000. На 3000 иногда не успевает.

--- server/rpd/rpd.ino
+++ server/rpd/rpd.ino
@@ -41,7 +41,7 @@
 
 //const byte DNS_PORT = 53;
 
-#define SAMPLE_RATE 3000 // (SAMPLE_RATE * 12) / 16 должно являться целым числом.
+#define SAMPLE_RATE 2000 // (SAMPLE_RATE * 12) / 16 должно являться целым числом.
 #if (SAMPLE_RATE * 12) & 0x0f
 #   error "The data will not fit into the buffer entirely."
 #endif
