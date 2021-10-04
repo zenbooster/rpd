2fcb5083bfa02d7f5aad95f25c37335b405af396
Igoncharov <igoncharov@aladdin.ru>
2021-10-04 16:18:46 +0300

    process.py: нормализуем фишки, фильтруем вычисляя скользящие средние.

--- client/process.py
+++ client/process.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 from struct import unpack
-import matplotlib
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import scipy
@@ -22,21 +20,31 @@ epoche_seconds = 10
 lowcut = 12
 highcut=40
 
-matplotlib.use('Qt5Agg')
+features = {
+    'iv': [],
+    'var': [],
+    'e': []
+}
+sfeatures = {
+    'iv': [],
+    'var': [],
+    'e': []
+}
+ftsize = 0
 
 with open('2021-10-02-23-43-50.myoblue.rpd', 'rb') as f:
     d = f.read(16)
     if d:
     #if True:
-        for i in range(105):
+        for i in range(150):
         #i = 0
         #while True:
             d = f.read(epoche_seconds*SAMPLE_RATE*2)
             if not d:
                 break
             
-            if i < 99:
-                continue
+            #if i < 99:
+            #    continue
                 
             print('epoche#{}'.format(i))
 
@@ -44,28 +52,17 @@ with open('2021-10-02-23-43-50.myoblue.rpd', 'rb') as f:
             t = unpack(fmt, d)
             d = list(t)
             d = np.array(d)
-            #d = signal_resample(d, sampling_rate=SAMPLE_RATE, desired_sampling_rate=RESAMPLE_RATE)
             d = d / (1 << 14)#(np.amax(d)*2)
         
             print(len(d))
             df=pd.DataFrame({'signal': d, 'id': [x*2 for x in range(len(d))]})
             df = df.set_index('id')
-            '''
-            nk.signal_plot(df)
-            fig = plt.gcf()
-            fig.savefig("process1.png", dpi=300)
-            '''
             
             d = df.signal
+            # подготовка сигнала:
             d = butter_bandpass_filter(d, lowcut, highcut, SAMPLE_RATE, order=2)
-            '''
-            df=pd.DataFrame({'signal': d, 'id': [x*2 for x in range(len(d))]})
-            df = df.set_index('id')            
-            nk.signal_plot(df)
-            fig = plt.gcf()
-            fig.savefig("process2.png", dpi=300)
-            '''
 
+            # извлекаем фишки (характеристики) сигнала:
             future_iv = np.mean(np.absolute(d))
             print('iv={}'.format(future_iv))
             future_var = np.var(d)
@@ -75,5 +72,41 @@ with open('2021-10-02-23-43-50.myoblue.rpd', 'rb') as f:
             df = pd.DataFrame({'signal': np.absolute(d)})
             future_e = df.rolling(ws).sum()[ws-1:].max().signal
             print('E={}'.format(future_e))
+
+            # нормализунем фишки:
+            features['iv'].append(future_iv)
+            features['var'].append(future_var)
+            features['e'].append(future_e)
             
+            sfeatures['iv'].append(future_iv)
+            sfeatures['iv'].sort()
+            sfeatures['var'].append(future_var)
+            sfeatures['var'].sort()
+            sfeatures['e'].append(future_e)
+            sfeatures['e'].sort()
+
+            ftsize += 1
+
+            N = 50
+            for k in range(10, 0, -1):
+                if ftsize > 2*N*k:
+                    for k, v in features.items():
+                        sf = sfeatures[k]
+                        Xmin = np.array(sf[:N]).mean()
+                        Xmax = np.array(sf[-N:]).mean()
+
+                        nf = []                    
+                        divider = Xmax - Xmin
+                        for v in v:
+                            nf.append((v - Xmin) / divider)
+
+                        # вычисляем пороги:
+                        # фильтруем фишки вычисляя скользящие средние:
+                        N = 4
+                        nff = np.convolve(nf, np.ones(N)/N, mode='valid')
+                        # ищем пики:
+                        print(nff)
+
+                    break
+
             #i+= 1
