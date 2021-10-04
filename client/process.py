268380d36889ce4cdba8dd199b1bfcbff49f4b72
Igoncharov <igoncharov@aladdin.ru>
2021-10-04 13:46:05 +0300

    process.py: научились считать энергию, отвязались от бесполезного neurokit2

--- client/process.py
+++ client/process.py
@@ -2,15 +2,25 @@
 from struct import unpack
 import matplotlib
 import matplotlib.pyplot as plt
-import neurokit2 as nk
-from neurokit2.signal import signal_resample
 import numpy as np
 import pandas as pd
+import scipy
+from scipy.signal import butter, sosfiltfilt
 import time
 
+def butter_bandpass_filter(data, lowcut, highcut, fs, order=2):
+    low = lowcut
+    high = highcut
+    sos = butter(order, [low, high], btype='bandpass', output="sos", fs=fs)
+    y = sosfiltfilt(sos, data)
+    return y
+    
 #SAMPLE_RATE=2000
 SAMPLE_RATE=500
 RESAMPLE_RATE = 500
+epoche_seconds = 10
+lowcut = 12
+highcut=40
 
 matplotlib.use('Qt5Agg')
 
@@ -18,14 +28,14 @@ with open('2021-10-02-23-43-50.myoblue.rpd', 'rb') as f:
     d = f.read(16)
     if d:
     #if True:
-        for i in range(39):
+        for i in range(105):
         #i = 0
         #while True:
-            d = f.read(30*SAMPLE_RATE*2)
+            d = f.read(epoche_seconds*SAMPLE_RATE*2)
             if not d:
                 break
             
-            if i < 28:
+            if i < 99:
                 continue
                 
             print('epoche#{}'.format(i))
@@ -34,7 +44,7 @@ with open('2021-10-02-23-43-50.myoblue.rpd', 'rb') as f:
             t = unpack(fmt, d)
             d = list(t)
             d = np.array(d)
-            d = signal_resample(d, sampling_rate=SAMPLE_RATE, desired_sampling_rate=RESAMPLE_RATE)
+            #d = signal_resample(d, sampling_rate=SAMPLE_RATE, desired_sampling_rate=RESAMPLE_RATE)
             d = d / (1 << 14)#(np.amax(d)*2)
         
             print(len(d))
@@ -47,7 +57,7 @@ with open('2021-10-02-23-43-50.myoblue.rpd', 'rb') as f:
             '''
             
             d = df.signal
-            d = nk.signal_filter(d, lowcut=12, highcut=40, method='butterworth', order=2)
+            d = butter_bandpass_filter(d, lowcut, highcut, SAMPLE_RATE, order=2)
             '''
             df=pd.DataFrame({'signal': d, 'id': [x*2 for x in range(len(d))]})
             df = df.set_index('id')            
@@ -56,13 +66,14 @@ with open('2021-10-02-23-43-50.myoblue.rpd', 'rb') as f:
             fig.savefig("process2.png", dpi=300)
             '''
 
-            #mean = np.mean(d)
-            var = np.var(d)
-            print('var={}'.format(var))
-            #d = nk.signal_sanitize(df.signal)
-            #d = nk.emg_clean(d, sampling_rate=RESAMPLE_RATE)
-            #amp = nk.emg_amplitude(d)
-            #print('amp={}'.format(amp))
+            future_iv = np.mean(np.absolute(d))
+            print('iv={}'.format(future_iv))
+            future_var = np.var(d)
+            print('var={}'.format(future_var))
             
-            #plot = df.plot(subplots=True, layout=(1, 1), color=['#f44336'])
+            ws = 6
+            df = pd.DataFrame({'signal': np.absolute(d)})
+            future_e = df.rolling(ws).sum()[ws-1:].max().signal
+            print('E={}'.format(future_e))
+            
             #i+= 1
